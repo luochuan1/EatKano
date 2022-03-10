b1806431bbdd52feb402c6a7a38e9ba52480d4da
luochuan1 <101337527+luochuan1@users.noreply.github.com>
2022-03-10 21:38:19 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,11 +441,11 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
-        return '人？';
+        if (cps <= 5) return '宇宙最屑';
+        if (cps <= 8) return '屑中之屑';
+        if (cps <= 10)  return '鸡汤来喽';
+        if (cps <= 15) return '蚌埠住了';
+        return '先辈？';
     }
 
     function toStr(obj) {
